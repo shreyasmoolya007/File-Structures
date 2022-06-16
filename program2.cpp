a19296e3c691edebbfbf44210bfc91c2164f6100
Shreyas Moolya <65644820+shreyasmoolya007@users.noreply.github.com>
2022-06-16 10:28:15 +0530

    Update program2.cpp

--- program2.cpp
+++ program2.cpp
@@ -39,18 +39,77 @@ class program2
         }
         
     }
+    
+     void unpack(fstream &ifile)
+    {
+        char extra[45];
+        while(!ifile.eof())
+        {
+            ifile.getline(name,20,'|');
+            ifile.getline(sem,10,'|');
+            ifile.getline(branch,10,'|');
+            ifile.getline(extra,45,'\n');
+            cout<<name<<" "<<sem<<" "<<branch<<" "<<extra<<endl; 
+        }
+        
+    }
+    
+    int search(fstream &ifile, char key[20]){
+        char extra[45];
+        while(!ifile.eof()){
+            ifile.getline(name,10,'|');
+            ifile.getline(sem,10,'|');
+            ifile.getline(branch,10,'|');
+            ifile.getline(extra,45,'\n');
+            if(strcmp(name,key)==0){
+                cout<<"Record Found : ";
+                cout<<name<<" "<<sem<<" "<<branch<<"\n";
+                return 1;
+            }
+        }
+        return 0;
+    }
 };
-
 
 int main()
 {
-    fstream ofile;
+    fstream ofile,ifile;
     string filename;
+    int ch;
+    char key[20];
     program2 st;
-    cout<<"Enter the filename\n";
-    cin>>filename;
-    ofile.open(filename,ios::out | ios::app);
-    st.pack(ofile);
-    ofile.close();
-    return 0;
+    
+    while(1)
+    {
+        
+        cout<<"1]Packing\n2]Unpacking\n3]Search\n4]Exit\n";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:cout<<"Enter the filename\n";
+                   cin>>filename;
+                   ofile.open(filename,ios::out | ios::app);
+                   st.pack(ofile);
+                   ofile.close();
+                   break;
+            case 2:cout<<"Enter the filename\n";
+                   cin>>filename;
+                   ifile.open(filename,ios::in);
+                   st.unpack(ifile);
+                   ifile.close();
+                   break;
+            case 3:cout<<"Enter the filename\n";
+                   cin>>filename;
+                   cout<<"Enter the record you want to search\n";
+                   cin>>key;
+                   ifile.open(filename,ios::in);
+                  if(st.search(ifile,key)==0){
+                      cout<<"Record not found ";
+                  }
+                  ifile.close();
+                  break;
+
+            case 4:exit(0);
+        }
+    }
 }
